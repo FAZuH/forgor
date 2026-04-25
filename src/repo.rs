e979c9643a3c4261dcdf8fc0efeb30303f411d18
FAZuH <mail@fazuh.com>
2026-04-25 16:08:56 +0700

    feat(db): add database models

--- src/repo.rs
+++ src/repo.rs
@@ -1,1 +1,7 @@
+use diesel_migrations::EmbeddedMigrations;
+use diesel_migrations::embed_migrations;
+
+pub mod model;
 pub mod schema;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
