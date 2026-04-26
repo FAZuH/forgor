47154250bc24a2af21b6ef18116fd0ceec8314d6
FAZuH <mail@fazuh.com>
2026-04-26 21:11:08 +0700

    refactor(ui): remove stale settings api

--- src/ui/pages/settings.rs
+++ src/ui/pages/settings.rs
@@ -28,53 +28,10 @@ pub enum SettingsMsg {
 }
 
 impl SettingsMsg {
-    pub fn is_toggle(&self) -> bool {
-        Self::is_toggle_index(self.index() as u32)
-    }
-
     pub fn is_toggle_index(index: u32) -> bool {
         (4..=6).contains(&index)
     }
 
-    /// Converts [`SettingsActions`] into index.
-    ///
-    /// Returns [`usize::MAX`] if [`SettingsActions::Navigate`].
-    pub fn index(&self) -> usize {
-        match self {
-            Self::TimerFocus(_) => 0,
-            Self::TimerShort(_) => 1,
-            Self::TimerLong(_) => 2,
-            Self::TimerLongInterval(_) => 3,
-            Self::TimerAutoFocus => 4,
-            Self::TimerAutoShort => 5,
-            Self::TimerAutoLong => 6,
-            Self::HookFocus(_) => 7,
-            Self::HookShort(_) => 8,
-            Self::HookLong(_) => 9,
-            Self::SoundFocus(_) => 10,
-            Self::SoundShort(_) => 11,
-            Self::SoundLong(_) => 12,
-        }
-    }
-
-    pub fn from_index(index: usize) -> Option<Self> {
-        match index {
-            0 => Some(Self::TimerFocus(Duration::default())),
-            1 => Some(Self::TimerShort(Duration::default())),
-            2 => Some(Self::TimerLong(Duration::default())),
-            3 => Some(Self::TimerLongInterval(0)),
-            4 => Some(Self::TimerAutoFocus),
-            5 => Some(Self::TimerAutoShort),
-            6 => Some(Self::TimerAutoLong),
-            7 => Some(Self::HookFocus(String::new())),
-            8 => Some(Self::HookShort(String::new())),
-            9 => Some(Self::HookLong(String::new())),
-            10 => Some(Self::SoundFocus(None)),
-            11 => Some(Self::SoundShort(None)),
-            12 => Some(Self::SoundLong(None)),
-            _ => None,
-        }
-    }
 }
 
 pub struct SettingsUpdate {}
