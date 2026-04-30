33bca2b27e1328e2a52f011d5555bbcc351a1f47
FAZuH <mail@fazuh.com>
2026-04-30 23:12:39 +0700

    feat(ui): add view traits

--- src/ui/traits.rs
+++ src/ui/traits.rs
@@ -9,3 +9,34 @@ pub trait Updateable {
     type Cmd;
     fn update(&mut self, msg: Self::Msg) -> Self::Cmd;
 }
+
+pub trait View<C> {
+    type Result;
+    fn render(&self, canvas: C) -> Self::Result;
+
+    fn render_mut(&mut self, canvas: C) -> Self::Result {
+        self.render(canvas)
+    }
+}
+
+pub trait StatefulView<C> {
+    type State;
+    type Result;
+    fn render_stateful(&self, canvas: C, state: &mut Self::State) -> Self::Result;
+
+    fn render_stateful_mut(&mut self, canvas: C, state: &mut Self::State) -> Self::Result {
+        self.render_stateful(canvas, state)
+    }
+}
+
+impl<T, C> StatefulView<C> for T
+where
+    T: View<C>,
+{
+    type State = ();
+    type Result = <T as View<C>>::Result;
+
+    fn render_stateful(&self, canvas: C, _state: &mut Self::State) -> Self::Result {
+        self.render(canvas)
+    }
+}
