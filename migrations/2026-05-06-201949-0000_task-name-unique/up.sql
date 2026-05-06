567ffa4426e83b1dcef14706508301f415b1b0a2
FAZuH <mail@fazuh.com>
2026-05-07 03:22:53 +0700

    feat(db): make tasks.name unique

---
+++ migrations/2026-05-06-201949-0000_task-name-unique/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tasks_new (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    deadline TIMESTAMP,
+    parent_id INTEGER,
+    project_id INTEGER,
+    FOREIGN KEY(parent_id) REFERENCES tasks_new(id) ON DELETE CASCADE,
+    FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET NULL
+);
+
+INSERT INTO tasks_new SELECT * FROM tasks;
+DROP TABLE tasks;
+ALTER TABLE tasks_new RENAME TO tasks;
